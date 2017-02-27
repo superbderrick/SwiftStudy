06c8ae7de1377f5935ef3d345f63fa77d91a073c
Derrick <kang.derrick@gmail.com>
2017-02-27 15:10:26 +0900

    first playground to study swift with swift tour

---
+++ swift_tour/SwiftTour.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//Simple Values
+
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+
+let implicitInter = 70
+let implicitDouble = 70.0
+let explicitDouble:Double = 70
+let explicitFloat:Float = 70.0
+
+let label = "The width is"
+let width = 94
+let widthLabel = label + String(width)
+let widthLabelWithCon = label + "\(width)"
+
+let apples = 3
+let oranges = 5
+
+let appleSummary = "I have \(apples)"
+let fruitSummary = "I have \(apples + oranges) pieces of fruit"
+
+let introduction = "HI Guys"
+let age:Float = 20
+var sayHello = "\(introduction) my age is \(age)"
