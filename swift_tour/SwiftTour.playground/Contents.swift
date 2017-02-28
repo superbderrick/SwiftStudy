67b02762eb7ebe5c13e17a4e07d84df5b4777340
Derrick <kang.derrick@gmail.com>
2017-02-28 20:32:51 +0900

    studied by page 13 next timew do about while

--- swift_tour/SwiftTour.playground/Contents.swift
+++ swift_tour/SwiftTour.playground/Contents.swift
@@ -30,3 +30,105 @@ let fruitSummary = "I have \(apples + oranges) pieces of fruit"
 let introduction = "HI Guys"
 let age:Float = 20
 var sayHello = "\(introduction) my age is \(age)"
+
+var shoppingList = ["catfish" , "water" , "tulips" , "blue paint"]
+
+shoppingList[0] = "derrick"
+
+var occupations = [
+  "Malcolm": "Captain",
+  "Kaylee": "Mechanic",
+]
+
+occupations["Jane"] = "Public Relations"
+
+let emptyArray = [String]()
+let emptyDict = [String:Float]()
+
+
+
+//Control FLow 
+
+
+
+let individualScores = [76,54,43,32]
+var tempScore = 0
+
+for score in individualScores {
+  if score > 50 {
+    tempScore += 3
+  } else {
+    tempScore += 1
+  }
+}
+
+print(tempScore)
+
+
+//optional
+
+var optionalString: String? = nil
+
+print(optionalString == nil)
+
+var optionName:String? = "John"
+var getting = "Hello"
+
+if let name = optionName  {
+  getting = "Hello \(name)"
+}
+
+print(getting)
+
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+  greeting = "Hello, \(name)"
+}
+
+print(greeting)
+
+let nickName: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+print(informalGreeting)
+
+
+
+
+let vegetable = "red peper"
+switch vegetable {
+case "celery":
+  
+  print("Add some raisins and make ants on a log ")
+case let x where x.hasSuffix("peper"):
+  print("Is it a spicy \(x)?")
+default:
+  print("Everything is fine")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
