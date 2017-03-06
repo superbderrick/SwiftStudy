23a4d97515c34f65baeb2477e4ea8e493c13fa30
Derrick <kang.derrick@gmail.com>
2017-03-06 21:21:13 +0900

    added object sample code by 27 page

--- objectAndClass/objectsAndClass.playground/Contents.swift
+++ objectAndClass/objectsAndClass.playground/Contents.swift
@@ -39,3 +39,80 @@ class NamedShape {
 var shaple = NamedShape("test")
 shaple.numberOfSides = 10
 shaple.simpleDescription()
+
+class Circle:NamedShape {
+  var redian:Double
+  init(_ redian:Double , name: String) {
+    self.redian = redian
+    super.init(name)
+  }
+  
+  override func simpleDescription() -> String {
+    return "A square with sides of length \(self.redian)"
+  }
+}
+
+var circle = Circle(3.5 , name: "test")
+circle.simpleDescription()
+
+class Square:NamedShape {
+  var sideLength:Double
+  
+  init(sideLength:Double , name:String) {
+    self.sideLength = sideLength
+    super.init(name)
+    numberOfSides = 4
+  }
+  
+  func area() -> Double {
+    return sideLength * sideLength
+  }
+  
+  override func simpleDescription() -> String {
+    return "A square with sides of length \(sideLength)"
+  }
+}
+
+let test = Square(sideLength:5.2 , name: "myTest square")
+test.area()
+test.simpleDescription()
+
+
+
+class EquilateralTriangle:NamedShape {
+  var sideLength:Double = 0.0
+  
+  init(sideLength:Double , name:String) {
+    self.sideLength = sideLength
+    super.init(name)
+    numberOfSides = 3
+  }
+  
+  var perimeter:Double {
+    get {
+      return 3.0 * sideLength
+    }
+    
+    set {
+      sideLength = newValue / 3.0
+    }
+  }
+  
+  override func simpleDescription() -> String {
+    return "An equilateral triangle with sides of length \(sideLength)"
+  }
+}
+
+
+var triangle = EquilateralTriangle(sideLength: 3.1 , name: "Derrick")
+print(triangle.perimeter)
+triangle.perimeter = 9.9
+print(triangle.sideLength)
+
+
+
+
+
+
+
+
