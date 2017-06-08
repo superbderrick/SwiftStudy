2ede42031e658d723ccca8469623d4d3c082b780
derrick <derrick.kang@nexstreaming.com>
2017-06-08 09:09:21 +0900

    added by controlflow

--- ControlFlow/controlFlow.playground/Contents.swift
+++ ControlFlow/controlFlow.playground/Contents.swift
@@ -44,16 +44,58 @@ let informalGreeting = "HI \(nickName ?? fullName)"
 
 
 
-let vegetable = "red pepper"
-switch vegetable {
-case "celery":
-	print("Add some raisins and make ants on a log.")
-case "cucumber" , "watercress":
-	print("That would make a good tea sandwich.")
-	
-case let x where x.hasSuffix("peper"):
-	print("Is it a spicy \(x)? ")
-	
-default:
-	print("Everything tastes good in soup")
+
+
+let interestingNumbers = [
+	"Prime" : [2,3,4,5,8,6] ,
+	"Fibonacci" : [1,1,2,3,5,8] ,
+	"Square" : [1,4,9,16,5]
+]
+
+var largest = 0
+
+for(kind , numbers) in interestingNumbers {
+	for number in numbers {
+		if number > largest {
+			largest = number
+		}
+	}
 }
+
+var maxValue: Int = 0
+
+let testValue = [1,5,8,7,6,9]
+
+for value in testValue {
+	if value > testValue {
+		maxValue = value;
+	}
+}
+
+print(maxValue)
+
+
+var n = 2
+while n < 100 {
+	n *= 2
+}
+
+var m = 2
+
+repeat {
+	m*=2
+} while m < 100
+print(m)
+
+
+
+var total = 0
+
+for i in 0..<4 {
+	total += i
+}
+
+
+
+
+
