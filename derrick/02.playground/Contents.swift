2c86ea3212553862dcb7aedb182798407b848c7e
Derrick <kang.derrick@gmail.com>
2020-09-20 18:21:27 +0900

    added 02

---
+++ derrick/02.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var myAge: Int = 31
+
+print(myAge)
+
+var anydictionary: Dictionary<String , Any> = [String:Any]()
+anydictionary["key"] = "test"
+
+print(anydictionary["key"])
+
+var someAny:Any
+
+var someAnyObject :AnyObject
+
+var integers: Array<Int> = Array<Int>()
+
+integers.append(1)
+integers.append(2)//ADD
+integers.removeAll()
+
+
+
